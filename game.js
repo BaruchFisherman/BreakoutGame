357843f17e405db361bd4ee6b40afb03f00b20c6
BaruchFisherman <baruch.fisherman@888holdings.com>
2016-09-24 23:54:41 +0300

    ball and paddle objects

--- game.js
+++ game.js
@@ -7,7 +7,25 @@ let ball = {
     dx: 2,
     dy: -2,
     radius: 10,
-    color: "red"
+    color: "red",
+    drow() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    },
+    setPosition() {
+        if (this.x + this.dx > canvas.width - this.radius || this.x + this.dx < this.radius) {
+            ball.dx *= -1;
+        }
+        if (this.y + this.dy > canvas.height - this.radius || this.y + this.dy < this.radius) {
+            this.dy *= -1;
+        }
+
+        this.x += this.dx;
+        this.y += this.dy;
+    }
 }
 
 
@@ -28,35 +46,13 @@ let paddle = {
 }
 
 
-function setBallPosition() {
-    if (ball.x + ball.dx > canvas.width - ball.radius || ball.x + ball.dx < ball.radius) {
-        ball.dx *= -1;
-    }
-    if (ball.y + ball.dy > canvas.height - ball.radius || ball.y + ball.dy < ball.radius) {
-        ball.dy *= -1;
-    }
-
-    ball.x += ball.dx;
-    ball.y += ball.dy;
-}
-
-function drawBall() {
-    ctx.beginPath();
-    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
-    ctx.fillStyle = ball.color;
-    ctx.fill();
-    ctx.closePath();
-}
-
-
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
+    ball.drow();
     paddle.draw();
-    setBallPosition();
+    ball.setPosition();
 
     requestAnimationFrame(draw);
 }
 
 draw();
-//setInterval(draw, 10);
